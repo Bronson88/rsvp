b906185c6815217751a20f9b5006f25eda5d4f26
Bronson <brocho8@gmail.com>
2017-09-15 22:25:37 -0600

    finishing up app

--- src/GuestList.js
+++ src/GuestList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import PendingGuest from './PendingGuest';
 import Guest from './Guest';
 
 const GuestList = props =>
   <ul>
+    <PendingGuest name={props.pendingGuest} />
     {props.guests
       .filter(guest => !props.isFiltered || guest.isConfirmed)
       .map((guest, index) =>
@@ -16,6 +18,7 @@ const GuestList = props =>
         handleConfirmation ={() => props.toggleConfirmationAt(index)}
         handleToggleEditing ={() => props.toggleEditingAt(index)}
         setName={text => props.setNameAt(text, index)}
+        handleRemove={() => props.removeGuestAt(index)}
       />
     )}
   </ul>;
@@ -25,7 +28,9 @@ GuestList.propTypes = {
   toggleConfirmationAt: PropTypes.func.isRequired,
   toggleEditingAt: PropTypes.func.isRequired,
   setName: PropTypes.func.isRequired,
-  isFiltered: PropTypes.bool.isRequired
+  isFiltered: PropTypes.bool.isRequired,
+  removeGuestAt: PropTypes.func.isRequired,
+  pendingGuest: PropTypes.string.isRequired
 }
 
 export default GuestList;
